ae5726bd6b20a2e4098ddd6c3a3f6f67d8ee30ad
lukepadlp <lukepadlp@gmail.com>
2017-01-07 18:39:33 -0600

    millis() no longer constant counter in loop
    Millis() does not run constantly anymore in the loop and prevent the
    loop from looping

--- DataLogger/DataLogger.ino
+++ DataLogger/DataLogger.ino
@@ -21,7 +21,9 @@
   * MISO Pin: 12
   * Clock Pin: 13
   * SDA and SCL are used for the ADS1115.
-  * Peristaltic Pump (PP) Pin: 3. Note: PWN is required for speed adjustment.
+  * Peristaltic Pump (PP) Pin: 3. Note: Adjust voltage on supply 
+  * to adjust the speed of the motor. It's cheaper than creating 
+  * a voltage divider with digital potentiometers.
   */
 
 //The ADS1015 library facilitates reading from the ADS1115.
@@ -30,13 +32,26 @@
 Adafruit_ADS1115 ads1115; //Creates instance of ads1115
 Adafruit_ADS1115 ads(0x48); //Sets base address
 //Create variables for displaying on LCD and saving to SD Card
-float cVoltage; //Variable reading from ADC's A0
+const String voltage = "mV"; //Change as needed
+const String conductivityText = "uS"; //Change as needed
+const String timeText = "ms"; //Change as needed
+double cVoltage; //Variable reading from ADC's A0
 String resultmV; //String containing reading from ADC in mV
 String resultS; //String variable containing result in microsiemens.
-//Note: Voltage in mV for 1000uS/cm conductivity is mV.
+const double intercept = 0; //Intercept for conductivity meter, provided by Vernier*
+const double slope = 0.357142857; //Slope for conductivity meter, provided by Vernier*
+//*THE INTERCEPT AND SLOPE ARE NOT YET CORRECT, STILL WAITING ON A RESPONSE
+//FROM VERNIER SUPPORT. 
+/*Note: Voltage in mV for 1000uS/cm conductivity is mV.
 //CALIBRATE USING THAT VOLTAGE. Ensure the Vernier conductivity meter's range is
-//set to 0 to 20,000 uS/cm (0 to 10,000 mg/L TDS).
+*set to 0 to 20,000 uS/cm (0 to 10,000 mg/L TDS).
+*/
 
+/*IMPORTANT NOTE REGARDING THE PUMP:
+* 40mL/minute speed is at 9v.
+* 20mL/minute speed is at 6v.
+* 10mL/minute speed is at 4v.
+*/
 
 //The LiquidCrystal library facilitates usage of the  LCD screen
 #include <LiquidCrystal.h>
@@ -49,7 +64,7 @@ LiquidCrystal lcd(4, 5, 9, 7, 6, 2); //Initialize LiquidCrystal Library
 SdFat SD; //Added for backwards compatability. This was suggested on the Arduino forum
 //by user Scottrockcafe to user trilbytim (https://forum.arduino.cc/index.php?topic=231495.0).
 //Create file for saving to SD Card
-String fileName = "test.txt"; //Change as needed
+String fileName = "calibration.txt"; //Change as needed
 #define SD_CS_PIN SS //Defines CS pin as SS for SD card
 SdFile file;
 
@@ -62,7 +77,19 @@ const int MOSIpin = 11; //MOSI pin
 const int MISOpin = 12; //MISO pin
 const int clk = 13; //Clock pin
 const int PP = 3; //Peristaltic pump pin
-const int pumpSpeed = 200; //Peristaltic pump speed (adjusted via PWM)
+boolean pumpOn = false; //boolean for turning pump on and off
+
+//Time-keeping variable
+//unsigned long time;
+
+//Set values for time it takes to circulate the 100mL 
+//bulk solution 5x through apparatus at pump speeds
+const int mlSpeed40 = 150000;
+const int mlSpeed20 = 300000;
+const int mlSpeed10 = 600000;
+
+//Record how many times water has been circulated through apparatus
+int cycle = 0;
 
 void setup() {
   //Set pin modes
@@ -78,16 +105,19 @@ void setup() {
   
   titleAndInitialize();
   LCDwrite("Beginning", "Logging");
+  delay(3000);
   lcd.clear();
   File file = SD.open(fileName, FILE_WRITE);
   if (file) {
-    file.println("testData"); //writes to SD card
+    file.println(conductivityText + "," + voltage + "," + timeText); //writes to SD card
     file.close();
   } else{
     LCDwrite("Error writing to", fileName);
+    while(1);
     }
-//    analogWrite(PP, pumpSpeed); //Sets speed of peristaltic pump to a constant value
-//    delay(100);
+    pumpOn = true;
+    digitalWrite(PP, HIGH); //Sets speed of peristaltic pump to a constant value
+    delay(100);
 }
 
 /*
@@ -101,7 +131,14 @@ void setup() {
  */
 void loop() {
   writeResult();
-  delay(500);
+  int ms = millis();
+  if (ms % mlSpeed40 == 0) { 
+  int cycle = cycle + 1; // Add 1 to cycle number
+    }
+  if (cycle >= 5){
+    LCDwrite("Data Collection", "Completed");
+    while(1);
+    }
 }
 
 void LCDwrite(String s, String s2){
@@ -124,25 +161,26 @@ void readADS() {
    * multiply that by 0.1875, and divide that by 1000 to get voltage
    * in volts.
    */
-  resultmV = cVoltage;
-  resultV = cVoltage/1000;
+   resultS = intercept + cVoltage*slope; //Fix later so this actually shows microsiemens
+   resultmV = cVoltage;
 }
 
 void SDwrite() {
   File file = SD.open(fileName, FILE_WRITE);
-  if (file) {
-    file.println(resultmV); //writes to SD card
-    file.close();
-  } else{
+  if (!file){
     LCDwrite("Error writing to", fileName);
-    delay(5000);
+    while(1);
     }
+    else {
+    file.println(resultS + "," + resultmV + "," + millis()); //writes to SD card
+    file.close();
+  }
 }
 
 void writeResult(){ //Writes result to SD card and LCD Screen
   readADS(); //Reads data from ADC
   SDwrite(); //Write data to SD card
-  LCDwrite((resultV + " V"), (resultmV + " mV ")); //Space keeps unnecessary "V" from appearing
+  LCDwrite((resultS + " V"), (resultmV + " mV")); //Space keeps unnecessary "V" from appearing
 }
 
 void titleAndInitialize(){
@@ -157,6 +195,7 @@ void titleAndInitialize(){
   LCDwrite("Detecting SD..", "");
   delay(500);
   LCDwrite("Detecting SD...", "");
+  delay(500);
   //Begin SD Card module
   if (!SD.begin(SD_CS_PIN)) {
     lcd.clear();
@@ -175,14 +214,8 @@ void titleAndInitialize(){
     LCDwrite("Error opening", fileName);
     while(1);
   }
+  file.close();
   LCDwrite("Writing to", fileName);
   delay(3000);
-  //Writes header to file
-  file.println("milliVolts");
-  file.println("");
   lcd.clear();
   }
-
-
-
-
